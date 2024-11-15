8a95a8d14f2ecaa4dcb410e9b4534b6b291547c4
RMnum18 <ratymael@gmail.com>
2024-11-15 13:25:20 +0100

    Update menu.php

--- menu/menu.php
+++ menu/menu.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="menu.css">
 <ul id="menu">
-<li><a href="page2.html">Accueille </a></li>
+<li><a href="page2.html">accueil </a></li>
 <li><a href="News.php">News </a></li>
 <li><a href="photo.php">Photos </a></li>
 <li><a href="contact.php">Contact </a></li>
