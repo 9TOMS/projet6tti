070ba6b96e81f7dcb11a123a02a12ea3b1948690
RMnum18 <ratymael@gmail.com>
2024-11-15 13:05:34 +0100

    Update menu.php

--- menu/menu.php
+++ menu/menu.php
@@ -1,1 +1,9 @@
-
+<link rel="stylesheet" href="menu.css">
+<ul id="menu">
+<li><a href="page2.html">Accueille </a></li>
+<li><a href="News.php">News </a></li>
+<li><a href="photo.php">Photos </a></li>
+<li><a href="contact.php">Contact </a></li>
+<li><a href="agenda.php">Agenda </a></li>
+<li><a href="tarif.php">Tarif </a></li>
+</ul>
