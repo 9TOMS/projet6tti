faa2a24e1de45487909cef803ca110988b28b51b
TybenjyX <110919623+TybenjyX@users.noreply.github.com>
2024-11-15 14:06:15 +0100

    Update index.php

--- index/index.php
+++ index/index.php
@@ -1,6 +1,14 @@
-<link rel="stylesheet" href="index.css"> 
-<title>Accueil</title>
-<div id="body">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+    <title>Accueil</title>
+</head>
+<body>
 <?php
     include("entete.php"); 
 ?>
@@ -29,4 +37,4 @@
 </div>
 <?php   include("pied_de_page.php"); 
 ?>
-</div>
+</body>
