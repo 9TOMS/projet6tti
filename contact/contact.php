02198fea8919204d13c96bd5dc8af637eda9d251
TOM <tomschrobi@gmail.com>
2024-11-15 14:20:47 +0100

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -1,1 +1,22 @@
-
+<link href="contact.css" rel="stylesheet">
+<?php include("tete.php"); ?>
+<div id = "corps">
+        <?php include("menu.php"); ?>
+    <div id="contact">
+        <h1>Contactez nous<h1>
+        <div id="MethodeDeContact">
+            <div id="telephone">
+                <img id ="ImageTelephone" src="gsm.png">
+                <h2>Contactez-nous par téléphone</h2>
+            </div>
+            <div id="mail">
+                <img id ="ImageMail" src="mail.png">
+                <h2>Contactez-nous par mail</h2>
+            </div>
+            <div id="facebook">
+                <img id ="ImageFacebook" src="facebook.png">
+                <h2>Contactez-nous par facebook</h2>
+            </div>
+        </div>
+    </div>
+</div>
