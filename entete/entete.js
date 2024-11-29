145f38889d785e7a2dea2e616f5f5ed79b78dba4
Vek0 <madomaged1227@gmail.com>
2024-11-29 14:04:16 +0100

    Add files via upload

---
+++ entete/entete.js
@@ -0,0 +1,19 @@
+// Fonction pour ouvrir le pop-up
+function openPopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex'; // Affiche le pop-up
+}
+
+// Fonction pour fermer le pop-up
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none'; // Cache le pop-up
+}
+
+// Optionnel : Fermer le pop-up en cliquant à l'extérieur
+window.addEventListener('click', (e) => {
+    const popup = document.getElementById('popup');
+    if (e.target === popup) {
+        popup.style.display = 'none';
+    }
+});
