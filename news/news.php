9cfc639013807b1735c6c3df87b4a93e237275f1
TybenjyX <110919623+TybenjyX@users.noreply.github.com>
2024-11-29 14:06:18 +0100

    Update news.php

--- news/news.php
+++ news/news.php
@@ -1,11 +1,17 @@
 <link rel="stylesheet" href="news.css">
-
+<script defer src="tarif.js"></script>
 <?php include("entete.php"); ?>
 
 <div class="Contenu">
     <?php include("menu.php"); ?>
-<?php include("includenews.php") ?>
- 
+<div id="grand_contenaire">
+    <!-- Flèche gauche -->
+    <button class="carousel-arrow left">&lt;</button>
+    <?php include("includenews.php") ?>
+    <?php include("includenews.php") ?>
+    <!-- Flèche droite -->
+        <button class="carousel-arrow right">&gt;</button>
+</div>
 </div>
 
 <?php include("pied_de_page.php"); ?>
