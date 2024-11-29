3b72860cf006f236e05991bbaf3d231cb1218a66
Maxime <maxime06.richard@gmail.com>
2024-11-29 13:19:52 +0100

    Update news.php

--- news/news.php
+++ news/news.php
@@ -4,51 +4,8 @@
 
 <div class="Contenu">
     <?php include("menu.php"); ?>
-
-    <div id="grand_contenaire">
-        <div class="news_poste marge">
-      
-            <div class="top">
-                <div class="gauche">Text</div>
-                <div class="droite">Date</div>
-            </div>
-            
-     
-            <div class="imagenews">
-                <img src="images/MenuFond.png" alt="Image de news" class="image">
-            </div>
-            
-            <div class="bas">
-                <div class="coeur">❤️</div>
-                <div class="partage">➤</div>
-            </div>
-            
-
-            <div class="commentaire">
-                <div class="commentaire-item">
-                    <div class="avatar"></div>
-                    <div class="texte">Trop beau le cheval</div>
-                    <div class="date">Date</div>
-                </div>
-                <div class="commentaire-item">
-                    <div class="avatar"></div>
-                    <div class="texte">Hâte de venir rencontrer ces chevaux...</div>
-                    <div class="date">Date</div>
-                </div>
-                <div class="commentaire-item">
-                    <div class="avatar"></div>
-                    <div class="texte">Comment on s'inscrit</div>
-                    <div class="date">Date</div>
-                </div>
-
-                <div class="ecrire-commentaire">
-                    <input type="text" placeholder="Écrire un commentaire...">
-                    <button>➤</button>
-                </div>
-            </div>
-        </div>
-    </div>
+<?php include("includenews.php") ?>
+ 
 </div>
 
 <?php include("pied_de_page.php"); ?>
-
