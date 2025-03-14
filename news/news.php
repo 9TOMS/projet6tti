30018946dcb748993edf542c93031fb807ce113c
TybenjyX <110919623+TybenjyX@users.noreply.github.com>
2025-03-14 13:17:43 +0100

    Update news.php

--- news/news.php
+++ news/news.php
@@ -1,17 +1,25 @@
 <link rel="stylesheet" href="news.css">
-<script defer src="tarif.js"></script>
+<script defer src="news.js"></script>
 <?php include("entete.php"); ?>
 
 <div class="Contenu">
     <?php include("menu.php"); ?>
-<div id="grand_contenaire">
-    <!-- Flèche gauche -->
-    <button class="carousel-arrow left">&lt;</button>
-    <?php include("includenews.php") ?>
-    <?php include("includenews.php") ?>
-    <!-- Flèche droite -->
-        <button class="carousel-arrow right">&gt;</button>
+    <div class="grand_contenaire">
+        <!-- Flèche gauche -->
+        <button class="carousel-arrow left" onclick="precedenteslide()">&lt;</button>
+        <div class="carousel">
+            <div class="carousel-items">
+                <?php 
+                for ($i = 1; $i <= 5; $i++) { 
+                    echo '<div class="news-item" id="news-' . $i . '">';
+                    include("includenews.php"); 
+                    echo '</div>';
+                }
+                ?>
+            </div>
+        </div>
+        <!-- Flèche droite -->
+        <button class="carousel-arrow right" onclick="prochaineslide()">&gt;</button>
+    </div>
 </div>
-</div>
-
 <?php include("pied_de_page.php"); ?>
