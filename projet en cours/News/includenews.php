ceb32db4c116655358820b9d03dc019c3cb26577
TOM <tomschrobi@gmail.com>
2025-03-21 13:36:35 +0100

    Add files via upload

---
+++ projet en cours/News/includenews.php
@@ -0,0 +1,41 @@
+        <div class="news_poste marge">
+      
+            <div class="top">
+                <div class="gauche">Text</div>
+                <div class="droite">Date</div>
+            </div>
+            
+     
+            <div class="imagenews">
+                <img src="MenuFond.png" alt="Image de news" class="image">
+            </div>
+            
+            <div class="bas">
+                <div class="coeur">❤️</div>
+                <div class="partage">➤</div>
+            </div>
+            
+
+            <div class="commentaire">
+                <div class="commentaire-item">
+                    <div class="avatar"></div>
+                    <div class="texte">Trop beau le cheval</div>
+                    <div class="date">Date</div>
+                </div>
+                <div class="commentaire-item">
+                    <div class="avatar"></div>
+                    <div class="texte">Hâte de venir rencontrer ces chevaux...</div>
+                    <div class="date">Date</div>
+                </div>
+                <div class="commentaire-item">
+                    <div class="avatar"></div>
+                    <div class="texte">Comment on s'inscrit</div>
+                    <div class="date">Date</div>
+                </div>
+
+                <div class="ecrire-commentaire">
+                    <input type="text" placeholder="Écrire un commentaire...">
+                    <button>➤</button>
+                </div>
+            </div>
+        </div>
