e379d0f4da9ba88fdfeb122b5149d1e149b34a27
TOM <tomschrobi@gmail.com>
2025-03-21 13:41:18 +0100

    Delete menu directory

--- menu/menu.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="menu.css">
-<ul id="menu">
-<li><a href="index.php">Accueil </a></li>
-<li><a href="News.php">News </a></li>
-<li><a href="photo.php">Photos </a></li>
-<li><a href="contact.php">Contact </a></li>
-<li><a href="agenda.php">Agenda </a></li>
-<li><a href="tarif.php">Tarif </a></li>
-</ul>
