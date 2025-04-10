590924c7c288f970329bc589080aa6c8241f0fe3
Maxime <maxime06.richard@gmail.com>
2025-04-10 10:51:05 +0200

    Update photo.php

--- projet en cours/Photo/photo.php
+++ projet en cours/Photo/photo.php
@@ -134,7 +134,7 @@
             right: 10%;
             position: relative;
         }
-        
+        /*animation supression*/
         .fade-out {
             opacity: 0;
             transform: scale(0.9);
