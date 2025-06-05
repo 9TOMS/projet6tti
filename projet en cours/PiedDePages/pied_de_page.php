c1ba4a097e763b66cb27f1c89016d9a20fc3973e
Maxime <maxime06.richard@gmail.com>
2025-06-05 10:13:23 +0200

    Update pied_de_page.php

--- projet en cours/PiedDePages/pied_de_page.php
+++ projet en cours/PiedDePages/pied_de_page.php
@@ -1,19 +1,7 @@
 <link rel="stylesheet" href="pied_de_page.css">
-<div class="div_pied_de_page">
-    <div>
-            <a href="aceuil.php">Accueil</a>   
-                •
-            <a href="Mentions légales.php">Mentions légales </a>
-                •
-            <a href="contacte.php">contact</a>
-    </div>
-    <div class="droit_div_pied_de_page">
-        |
-        <a href="https://www.facebook.com/asblcrepuscule">©crépuscule</a>
-        |
-        <a href="https://pierrard.be/">©pierrard</a> 
-
-    </div>
+<div id="pied_de_page">
+    <p><a href="index.php">Accueil</a>•<a href="mentions_legales.php">Mentions légales </a>•<a href="contact.php">contact</a></p>
+    <p>|<a target="_blank" href="https://www.facebook.com/asblcrepuscule">©crépuscule</a>|<a target="_blank" href="https://pierrard.be/">©pierrard</a> </p>
 </div>
 </body>
 </html>
