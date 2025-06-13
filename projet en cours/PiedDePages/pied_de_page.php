a3d22891d12438aafd17082c75e11f6d4da4116f
Maxime <maxime06.richard@gmail.com>
2025-06-13 11:30:44 +0200

    Add files via upload

--- projet en cours/PiedDePages/pied_de_page.php
+++ projet en cours/PiedDePages/pied_de_page.php
@@ -1,7 +1,4 @@
-<link rel="stylesheet" href="pied_de_page.css">
-<div id="pied_de_page">
+<footer id="pied_de_page">
     <p><a href="index.php">Accueil</a>•<a href="mentions_legales.php">Mentions légales </a>•<a href="contact.php">contact</a></p>
-    <p>|<a target="_blank" href="https://www.facebook.com/asblcrepuscule">©crépuscule</a>|<a target="_blank" href="https://pierrard.be/">©pierrard</a> </p>
-</div>
-</body>
-</html>
+    <p>|<a target="_blank" title="Page Facebook de Crépuscule" rel="noopener noreferrer" href="https://www.facebook.com/asblcrepuscule">©crépuscule</a>|<a target="_blank" rel="noopener noreferrer" title="Site internet de Pierrard" href="https://pierrard.be/">©pierrard</a> </p>
+</footer>
